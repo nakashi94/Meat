9e20ccb391dfbf88f29f37506c36840845aab45f
sorann <81224242+GureGorii@users.noreply.github.com>
2021-10-20 11:59:17 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
+# import module
 from flask import Flask, render_template
+from flask import Response, request, redirect, url_for
+from flask import send_from_directory, send_file
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, date
 
 app = Flask(__name__)
 
-@app.route('/', methods=['Get'])
+@app.route('/', methods=['Get', 'POST'])
 def index():
-    return "test"
+    if request.method == 'GET':
+    #return "test"
+        return render_template('home.html')
+    else:
+        return render_template('form.html')
 
-@app.route('/hello-world', methods=["Get"])
-def hello():
-    return render_template('index.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'GET':
+        return render_template('form.html')
+    else:
+        a = request.form.get('done')
+        b = request.form.get('do')
+
+        return render_template('register.html', a=a, b=b)
+
+#データベース格納
+@app.route('/register', methods=['GET', 'POST'])
+def resister():
+    if request.method == 'GET':
+        return render_template('sub.html')
+    else:
+        return render_template('sub.html')
+
+@app.route('/sub', methods=['GET', 'POST'])
+def sub():
+    if request.method == 'GET':
+        return render_template('sub.html')
+    else:
+        return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.debug = True
