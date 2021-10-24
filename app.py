b1ea126f8c3a8234a6bbcaf44d99304e8157c5d1
Rei-Suzuki1729 <eva2021end@gmail.com>
2021-10-24 11:19:23 +0900

    create progressform && reportform

--- app.py
+++ app.py
@@ -38,6 +38,18 @@ def list():
     return render_template("list-form.html", title=title)
 
 
+@app.route("/progressform", methods=["GET", "POST"])
+def progform():
+    title="進捗状況"
+    return render_template("progressform.html",title=title)
+
+
+@app.route("/reportform", methods=["GET", "POST"])
+def input():
+    title="日報"
+    return render_template("reportform.html",title=title)
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0", port=80)
